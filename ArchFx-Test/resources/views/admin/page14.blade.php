c6b118fd8aa654e0ccd35f4a7e47154a59945d9f
ArchSharp <56973306+ArchSharp@users.noreply.github.com>
2021-05-03 23:33:33 +0100

    Working on beginners

--- ArchFx-Test/resources/views/admin/page14.blade.php
+++ ArchFx-Test/resources/views/admin/page14.blade.php
@@ -4,8 +4,90 @@
     <h4 class="text-primary fw-bold">Remarks</h4>
     <div class="col justify-content-start text-left">
         <p>
-            
+           Now that you have gone through Beginners level, You are one step into trading. The sincere truth is not everyone have the time to sit down to trade. In my years of experience 
+           you need to be sufficiently equiped to trade Forex profitably, yes profitably not everyone that trade are profitable. Its no longer a news that we all need additional income and trading Forex 
+           is one of the option you can consider. But we understand that trading forex profitably may still be a challenge even after going through series of Forex training so we at <strong class="text-success">ArchFxGlobe Ltd</strong> 
+           have come up with a solution which is creating a profitable <span class="text-success fw-bold">ROBOT</span> for you. You can check our track record on <span class="text-primary">MQL5 Signal</span> to see 
+           for yourself. <br>
+           Depending on your capital strength, we have ROBOT that requires just $50 you can try that out. If you have been trading you would have know now that ROBOT that can trade $50 is very rare so consider this as a rare opportunity 
+           we also have Robot that requires capital up to $1,000,000.
         </p>
+        <table class="table table-responsive caption-top">
+            <caption><span class="fs-4">Major Currencies</span>  - These are the major currencies in the Forex market</caption>
+            <thead>
+              <tr>
+                <th scope="col">Requirements</th>
+                <th scope="col">AIsCapital</th>
+                <th scope="col">AImCapital</th>
+                <th scope="col">AIbCapital</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Price ( $ )</th>
+                <td>100</td>
+                <td>300</td>
+                <td>500</td>
+              </tr>
+              <tr>
+                <th scope="row">Monthly ROI</th>
+                <td>>= 10%</td>
+                <td>>= 10%</td>
+                <td>>= 10%</td>
+              </tr>
+              <tr>
+                <th scope="row">Minimum Capital ( $ )</th>
+                <td>50</td>
+                <td>300</td>
+                <td>500</td>
+              </tr>
+              <tr>
+                <th scope="row">Capital Protection</th>
+                <td><i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                </td>
+                <td><i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                </td>
+                <td><i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Verified by</th>
+                <td><a href="#">MyFxBook</a></td>
+                <td><a href="#">MyFxBook</a></td>
+                <td><a href="#">MyFxBook</a></td>
+              </tr>
+              <tr>
+                <th scope="row">6</th>
+                <td>CAD</td>
+                <td>Canada</td>
+                <td>Dollar</td>
+              </tr>
+              <tr>
+                <th scope="row">7</th>
+                <td>CHF</td>
+                <td>Switzerland</td>
+                <td>Franc</td>
+              </tr>
+              <tr>
+                <th scope="row">Buy and Smile</th>
+                <td><a href="#" class="btn btn-primary btn-sm" tabindex="-1" role="button">PAY WITH FLUTTERWAVE</a></td>
+                <td><a href="#" class="btn btn-primary btn-sm" tabindex="-1" role="button">PAY WITH FLUTTERWAVE</a></td>
+                <td><a href="#" class="btn btn-primary btn-sm" tabindex="-1" role="button">PAY WITH FLUTTERWAVE</a></td>
+              </tr>
+            </tbody>
+          </table>
         
         <!-- <iframe width="640" height="345" src="https://www.youtube.com/embed/ilFZ0FJnUJ4?autoplay=0&mute=0&controls=1">
         </iframe> -->
