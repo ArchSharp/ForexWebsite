3d122c50b94660cd929deeb87d6af157d444df0c
ArchSharp <56973306+ArchSharp@users.noreply.github.com>
2021-05-05 23:11:42 +0100

    another mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,9 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
-    /*.sass('resources/sass/app.scss', 'public/css')*/
-    .sass('resources/css/app.css', 'public/css')
+ mix.js('public/js/app.js', 'public/js')
+/*mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/css/app.css', 'public/css')*/
+    .css('public/css/app.css', 'public/css')
     .sourceMaps();
