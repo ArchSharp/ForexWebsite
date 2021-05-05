3f0c58d7bf5a5d582b2b64da6f74113b6f9c35eb
ArchSharp <56973306+ArchSharp@users.noreply.github.com>
2021-05-05 22:51:29 +0100

    just try

--- resources/views/layouts/dashPages/head.blade.php
+++ resources/views/layouts/dashPages/head.blade.php
@@ -16,8 +16,8 @@
       }
     </script>
     <!-- Scripts -->
-    <script src={{ asset("js/app.js") }} defer></script>
-    <script src={{ asset("js/archjs.js") }} defer></script>
+    {{-- <script src={{ asset("js/app.js") }} defer></script>
+    <script src={{ asset("js/archjs.js") }} defer></script> --}}
 
     <!-- Styles -->
     <link href={{ asset("css/app.css") }} rel="stylesheet">
