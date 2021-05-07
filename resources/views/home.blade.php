b24bb4e0f6f3bba80cd66e524be29927db599d51
ArchSharp <56973306+ArchSharp@users.noreply.github.com>
2021-05-07 04:26:32 +0100

    modal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -70,7 +70,7 @@
     <div class="modal-content">
       <div class="modal-header justify-content-center">
         <h5 class="modal-title" id="exampleModalLabel">Build your Indicator & Expert advisor</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        {{-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> --}}
       </div>
       <div class="modal-body">
         <form action="{{ route('auth.sendJob') }}" method="post" enctype="multipart/form-data" id="uploadfile">
