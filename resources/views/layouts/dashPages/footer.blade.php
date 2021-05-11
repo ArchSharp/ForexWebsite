68138b51105a6228288c66435dc51c4f9a392b7f
ArchSharp <56973306+ArchSharp@users.noreply.github.com>
2021-05-12 00:54:55 +0100

    policy

--- resources/views/layouts/dashPages/footer.blade.php
+++ resources/views/layouts/dashPages/footer.blade.php
@@ -5,7 +5,7 @@
         &copy; <?php
         $fromYear = 2018;
         $thisYear = (int)date('Y');
-        echo $fromYear . (($fromYear != $thisYear) ? '-' . $thisYear : '');?> ArchFxGlobe Ltd.
+        echo $fromYear . (($fromYear != $thisYear) ? '-' . $thisYear : '');?> <span class="text-primary fw-bold">ArchFxGlobe</span>.
       </span>
       <span class="ms-4"><a href="#" data-bs-toggle="modal" data-bs-target="#privacyModal">Privacy policy</a></span>
       <span class="ms-4"><a href="#" data-bs-toggle="modal" data-bs-target="#termsModal">Terms of reference</a></span>
@@ -27,17 +27,17 @@
             Every user is entitled to 2 MT4 account activation. In any case your previous account is no longer been used, we can deactivate the old MT4 account number and grant license on the new account. This offer is available only ones. <br> <br>
             PURPOSE OF THIS PRIVACY POLICY <br> <br>
 
-            This Policy aims to define the process how ARCHFXGLOBE collects and processes personal data of the Clients through the use of ARCHFXGLOBE.com (hereinafter referred to as the “Website”), including any data the Client may provide through the Website when providing or summiting any information thereto. <br>
+            This Policy aims to define the process how <span class="text-primary fw-bold"><span class="text-primary fw-bold">ArchFxGlobe</span></span> collects and processes personal data of the Clients through the use of <span class="text-primary fw-bold"><span class="text-primary fw-bold">ArchFxGlobe</span></span>.com (hereinafter referred to as the “Website”), including any data the Client may provide through the Website when providing or summiting any information thereto. <br>
             <br>
             This Website is not intended for children and no data related to children may be collected.
             <li> DATA CONTROLLER</li>
 
-            ARCHFXGLOBE is the controller and responsible person/entity for Client’s personal data. It is important that the personal data the Company holds about the Client is accurate and current. The obligation to inform the Company about any changes in personal data will be on Client’s side.
+            <span class="text-primary fw-bold"><span class="text-primary fw-bold">ArchFxGlobe</span></span> is the controller and responsible person/entity for Client’s personal data. It is important that the personal data the Company holds about the Client is accurate and current. The obligation to inform the Company about any changes in personal data will be on Client’s side.
             <li>DATA COLLECTED</li>
 
             Personal data, or personal information, means any information about an individual from which that person can be identified. It does not include data where the identity has been removed (anonymous data). The Company may collect, use, store and transfer different kinds of personal data about the Client which we have grouped together follows:
 
-            Identity Data includes first name, maiden name, last name, username or similar identifier.
+            Identity Data includes first name, last name, username or similar identifier.
             Contact Data includes email address and telephone numbers.
             Technical Data includes internet protocol (IP) address, Client’s login data, browser type and version, time zone setting and location, browser plug-in types and versions, operating system and platform and other technology on the devices the Client uses to access this website.
             Profile Data includes the username and password of the Client, his interests, preferences, feedback and survey responses.
@@ -53,16 +53,15 @@
             Where it is necessary for Company’s legitimate interests (or those of a third party) and if such interests and fundamental rights do not override these interests.
             Where the Company shall comply with a legal or regulatory obligation.
 
-            In general, the Company does not rely on respective consent, forming a legal basis for processing of Client’s personal data, other than in relation to sending third party direct marketing communications to you via email or text message. The Client has the right to withdraw his consent to marketing purposes at any time by contacting the Company via mail to info@ARCHFXGLOBE.com
+            In general, the Company does not rely on respective consent, forming a legal basis for processing of Client’s personal data, other than in relation to sending third party direct marketing communications to you via email or text message. The Client has the right to withdraw his consent to marketing purposes at any time by contacting the Company via mail to info@<span class="text-primary fw-bold"><span class="text-primary fw-bold">ArchFxGlobe</span></span>.com
             <li>PURPOSES FOR WHICH THE COMPANY USES PERSONAL DATA OF THE CLIENTS</li>
 
-            Client’s personal data may be processed for more than one lawful ground depending on the specific purpose for which we are using your data. If necessary, the Client shall contact the Company via mail to info@ARCHFXGLOBE.com for the purpose of obtaining necessary details about the specific legal ground the Company relies on to process his personal data where more than one ground has been set out.
+            Client’s personal data may be processed for more than one lawful ground depending on the specific purpose for which we are using your data. If necessary, the Client shall contact the Company via mail to info@<span class="text-primary fw-bold"><span class="text-primary fw-bold">ArchFxGlobe</span></span>.com for the purpose of obtaining necessary details about the specific legal ground the Company relies on to process his personal data where more than one ground has been set out.
             <li>DISCLOSURE OF CLIENT’S PERSONAL DATA</li>
 
-            The Company may share Client‘s personal data with third parties for the purpose of carrying out its services related to ARCHFXGLOBE properly. Security of Client‘s personal data shall be fully respected by such third parties and treated in accordance with the law. Such Client‘s personal data may not be used by third parties for their own purposes while they may be processed only for specified purposes by the Company and in accordance with Company‘s instructions. Such providers are: <br>
+            The Company may share Client‘s personal data with third parties for the purpose of carrying out its services related to <span class="text-primary fw-bold"><span class="text-primary fw-bold">ArchFxGlobe</span></span> properly. Security of Client‘s personal data shall be fully respected by such third parties and treated in accordance with the law. Such Client‘s personal data may not be used by third parties for their own purposes while they may be processed only for specified purposes by the Company and in accordance with Company‘s instructions. Such providers are: <br>
 
-            Paystack Inc. www.paystack.com
-            Coinbase Commerce Inc. www.coinbase.com
+            Flutterwave www.flutterwave.com
           </ol>
         </div>
       </div>
@@ -86,23 +85,23 @@
           
           <ol>
             <li>No professional advice or investment recommendation.</li>
-            Any recommendations on the website should not be considered as a solicitation to buy or an offer to sell. The recommendations are not individual recommendations to a specific user. ARCHFXGLOBE is not an investment adviser, therefore, any trade ideas or signals should not be taken as investment advice. The users are solely responsible for the investment decisions they make. Users should consult a tax professional or attorney to understand their tax or legal obligations.
+            Any recommendations on the website should not be considered as a solicitation to buy or an offer to sell. The recommendations are not individual recommendations to a specific user. <span class="text-primary fw-bold">ArchFxGlobe</span> is not an investment adviser, therefore, any trade ideas or signals should not be taken as investment advice. The users are solely responsible for the investment decisions they make. Users should consult a tax professional or attorney to understand their tax or legal obligations.
             <li>No refund policy.</li>
-            The licensed ARCHFXGLOBE indicator and scanner is a lifetime license activation product and cannot be deactivated once it has been licensed to your MT4 account number. The licensed cannot be transfered from one MT4 account number to the other. It is a virtual digital product that cannot be returned and therefore is nonrefundable.
+            We at <span class="text-primary fw-bold">ArchFxGlobe</span> have carefully designed all our indicators and Expert advisors (Robot). Our products is a lifetime license activation product and cannot be deactivated once it has been configured to your MT4 account number. The licensed cannot be transfered from one MT4 account number to the other. It is a virtual digital product that cannot be returned and therefore is nonrefundable.
             <li>Access to website.</li>
             The website, or any part of it, may not be duplicated, copied, used, or otherwise exploited for any commercial mean without our express, written permission. You may not use any framing techniques to acquire any logo, trademark or other proprietary content (textual, images, text, layouts or forms) of our website without our prior written approval.
             <li>Website policies.</li>
-            ARCHFXGLOBE reserves the right to make changes to the website, policies, and terms of use at any given time and does not need to inform its users when doing so. It is the responsibility of the members to check the privacy policy and terms of use frequently to see any recent changes.
+            <span class="text-primary fw-bold">ArchFxGlobe</span> reserves the right to make changes to the website, policies, and terms of use at any given time and does not need to inform its users when doing so. It is the responsibility of the members to check the privacy policy and terms of use frequently to see any recent changes.
             <li>Limited warranties.</li>
             We do not warrant the completeness or accuracy of the information published on this website; nor do we commit to ensuring that the website remains available or that the material on the website is kept up to date.
             <li>Limitations and exclusions of liability</li>
-            ARCHFXGLOBE will not be responsible to any fraud activities carried out by anybody direct or indirect with any product, item, material or information from this website. We are not liable for any wrong interpretation, distributing or sharing of information from this website. We will not be liable to you in respect of any losses arising out of any event or events beyond our reasonable control. We will not be liable to you in respect of any business losses, including (without limitation) loss of or damage to profits, income, revenue, use, production, anticipated savings, business, contracts, commercial opportunities or goodwill. Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law. Your use of any information or materials on this website is entirely at your own risk, for which we shall not be liable. It shall be your own responsibility to ensure that any products, services or information available through this website meet your specific requirements.
+            <span class="text-primary fw-bold">ArchFxGlobe</span> will not be responsible to any fraud activities carried out by anybody direct or indirect with any product, item, material or information from this website. We are not liable for any wrong interpretation, distributing or sharing of information from this website. We will not be liable to you in respect of any losses arising out of any event or events beyond our reasonable control. We will not be liable to you in respect of any business losses, including (without limitation) loss of or damage to profits, income, revenue, use, production, anticipated savings, business, contracts, commercial opportunities or goodwill. Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law. Your use of any information or materials on this website is entirely at your own risk, for which we shall not be liable. It shall be your own responsibility to ensure that any products, services or information available through this website meet your specific requirements.
             <li>High risk investment.</li>
             Trading foreign exchange on margin carries a high level of risk, and may not be suitable for all investors. The high degree of leverage can work against you as well as for you. Before deciding to trade foreign exchange, you should carefully consider your investment objectives, level of experience, and risk appetite. The possibility exists that you could sustain a loss of some or all of your initial investment and therefore you should not invest money that you cannot afford to lose. You should be aware of all the risks associated with foreign exchange trading, and seek advice from an independent financial advisor if you have any doubts.
             <li>Disclaimer.</li>
             All information posted on this website is of our opinion and the opinion of reviewers, and may not reflect the truth. Please use your own good judgment and use common sense as well as seeking advice from a qualified consultant before believing and accepting any information posted on this website.
             <li>Changes to this Statement.</li>
-            ArchFxGlobe.com will occasionally update this Terms of Service to reflect company and customer feedback. ARCHFXGLOBE.com encourages you to periodically review this Statement to be informed of how ARCHFXGLOBE.com is protecting your information. All of these terms are subject to change without notice. Changes will be posted on this page.
+            <span class="text-primary fw-bold">ArchFxGlobe</span>.com will occasionally update this Terms of Service to reflect company and customer feedback. <span class="text-primary fw-bold">ArchFxGlobe</span>.com encourages you to periodically review this Statement to be informed of how <span class="text-primary fw-bold">ArchFxGlobe</span>.com is protecting your information. All of these terms are subject to change without notice. Changes will be posted on this page.
           </ol>
         </div>
       </div>
