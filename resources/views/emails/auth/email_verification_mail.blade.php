6225fba1743b9afa082a30bdbb3e02b4faaca75c
ArchSharp <56973306+ArchSharp@users.noreply.github.com>
2021-05-11 14:13:11 +0100

    mail

--- resources/views/emails/auth/email_verification_mail.blade.php
+++ resources/views/emails/auth/email_verification_mail.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Email Verification</title>
-</head>
-
-<body>
+@component('mail::message')
 
 Hello {{$user->firstname}}, <br>
 @component('mail::panel')
@@ -26,10 +16,10 @@ Clcik here to verify your email address
         {{route('verify_email',$user->email_verification_code)}}
     </a>
 </p>
+<p>
+    Thank you.
+</p>
 
-Thanks,<br>
-{{ config('app.name') }}.
-
-</body>
-
-</html>
+Regards,<br>
+{{ config('app.name') }} Support.
+@endcomponent
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+<img src="../images/fxGlobe.gif" style="width:30px;" class="logo" alt="Logo">
 @else
 {{ $slot }}
 @endif
