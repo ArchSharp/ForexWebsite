fcbe3e6df82a64c2ddf28260ce822a67bd4a252e
ArchSharp <56973306+ArchSharp@users.noreply.github.com>
2021-05-11 14:28:54 +0100

    mail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
+@if (trim($slot) !== 'Laravel')
 <img src="https://archfxglobe.com/images/fxGlobe.gif" class="logo" alt="Logo">
 @else
 {{ $slot }}
