84e946182c0a480ecc273a04e8c168f854fa835f
ArchSharp <56973306+ArchSharp@users.noreply.github.com>
2021-05-13 00:40:39 +0100

    tutorial

--- resources/views/admin/page17.blade.php
+++ resources/views/admin/page17.blade.php
@@ -5,10 +5,103 @@
     <div class="col justify-content-start text-left">
         <p>
             Candlestick pattern usually form at a market price/level where market will most likely change direction. If you understand this definition, it simply means you don't 
-            always need to pay attention to every candlestick pattern. Only pay attention to candlestick pattern at a support or resistance level in the market.
+            always need to pay attention to every candlestick pattern. Only pay attention to candlestick pattern at a support or resistance level in the market. <br>
+
+            But before that, you need to know what a candlestick is. Candlestick is object that represent market price and its movement. Below image is a 
+            is a typical candlestick.
             
             <div class="d-flex justify-content-center mt-3">
-                <img src="{{ asset("../images/trendline.png") }}" alt="" style="width: 60%;">
+                <img src="{{ asset("../images/candlestick.jpg") }}" alt="" style="width: 60%;">
+            </div>
+
+            Green and Red are the typical color most people use to represent buy and sell candle. Green candle is a buy candle while red candle is a sell candle.
+            Notice that the open for buy candle is down while close is up. The open for sell candle is up while close is down. Open, Close, High and Low are all price on the chart.
+            <ul>
+                <li>Open is the price where the candle starts from.</li>
+                <li>Close is the price the candle close with.</li>
+                <li>High and Low are price where market got to but did not close there.</li>
+            </ul>
+
+            <div class="container">
+                <div class="row">
+                    <div class="carousel-inner">
+                        <div class="card-group">
+                            <div class="card">
+                            <!--<img src="..." class="card-img-top" alt="...">-->
+                            <img class="card-img-top" src="{{ asset("../images/inverted-hammer.png") }}" alt="Widget" style="width:100%; height: 100%;">
+                            {{-- <iframe frameborder="0" width="100%" height="140" src="https://www.mql5.com/en/signals/widget/signal/5cug?f=0"></iframe> --}}
+                            <div class="card-body">
+                                <h5 class="card-title">Details</h5>
+                                <p class="card-text">Few informations such as drawdown and current ROI% can be seen in above image, all informations you need to convince you about the profitability can be seen by clicking "See Results", it will take you MyFxBook.</p>
+                            </div>
+                            </div>
+                            <div class="card ms-2 border">
+                            <!--<img src="..." class="card-img-top" alt="...">-->
+                            <img class="card-img-top" src="{{ asset("../images/hanging-man.png") }}" alt="Widget" style="width:100%; height: 100%;">
+                            <div class="card-body">
+                                <h5 class="card-title">Details</h5>
+                                <p class="card-text">Few informations such as drawdown and current ROI% can be seen in above image, all informations you need to convince you about the profitability can be seen by clicking "See Results", it will take you MyFxBook.</p>
+                            </div>
+                            </div>
+                            <div class="card ms-2 border">
+                            <!--<img src="..." class="card-img-top" alt="...">-->
+                            <img class="card-img-top" src="{{ asset("../images/hammer.png") }}" alt="Widget" style="width:100%; height: 100%;">
+                            <div class="card-body">
+                                <h5 class="card-title">Details</h5>
+                                <p class="card-text">Few informations such as drawdown and current ROI% can be seen in above image, all informations you need to convince you about the profitability can be seen by clicking "See Results", it will take you MyFxBook.</p>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="card-group mt-2">
+                            <div class="card">
+                            <!--<img src="..." class="card-img-top" alt="...">-->
+                            <img class="card-img-top" src="{{ asset("../images/evening-star.png") }}" alt="Widget" style="width:100%; height: 100%;">
+                            {{-- <iframe frameborder="0" width="100%" height="140" src="https://www.mql5.com/en/signals/widget/signal/5cug?f=0"></iframe> --}}
+                            <div class="card-body">
+                                <h5 class="card-title">Details</h5>
+                                <p class="card-text">Few informations such as drawdown and current ROI% can be seen in above image, all informations you need to convince you about the profitability can be seen by clicking "See Results", it will take you MyFxBook.</p>
+                            </div>
+                            </div>
+                            <div class="card ms-2 border">
+                            <!--<img src="..." class="card-img-top" alt="...">-->
+                            <img class="card-img-top" src="{{ asset("../images/shooting-star.png") }}" alt="Widget" style="width:100%; height: 100%;">
+                            <div class="card-body">
+                                <h5 class="card-title">Details</h5>
+                                <p class="card-text">Few informations such as drawdown and current ROI% can be seen in above image, all informations you need to convince you about the profitability can be seen by clicking "See Results", it will take you MyFxBook.</p>
+                            </div>
+                            </div>
+                            <div class="card ms-2 border">
+                            <!--<img src="..." class="card-img-top" alt="...">-->
+                            <img class="card-img-top" src="{{ asset("../images/morning-star.png") }}" alt="Widget" style="width:100%; height: 100%;">
+                            <div class="card-body">
+                                <h5 class="card-title">Details</h5>
+                                <p class="card-text">Few informations such as drawdown and current ROI% can be seen in above image, all informations you need to convince you about the profitability can be seen by clicking "See Results", it will take you MyFxBook.</p>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="card-group mt-2">
+                            <div class="card">
+                            <!--<img src="..." class="card-img-top" alt="...">-->
+                            <img class="card-img-top" src="{{ asset("../images/bullish-engulfing.png") }}" alt="Widget" style="width:100%; height: 100%;">
+                            {{-- <iframe frameborder="0" width="100%" height="140" src="https://www.mql5.com/en/signals/widget/signal/5cug?f=0"></iframe> --}}
+                            <div class="card-body">
+                                <h5 class="card-title">Details</h5>
+                                <p class="card-text">Few informations such as drawdown and current ROI% can be seen in above image, all informations you need to convince you about the profitability can be seen by clicking "See Results", it will take you MyFxBook.</p>
+                            </div>
+                            </div>
+                            <div class="card ms-2 border">
+                            <!--<img src="..." class="card-img-top" alt="...">-->
+                            <img class="card-img-top" src="{{ asset("../images/bearish-engulfing.png") }}" alt="Widget" style="width:100%; height: 100%;">
+                            <div class="card-body">
+                                <h5 class="card-title">Details</h5>
+                                <p class="card-text">Few informations such as drawdown and current ROI% can be seen in above image, all informations you need to convince you about the profitability can be seen by clicking "See Results", it will take you MyFxBook.</p>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </p>
--- resources/views/admin/page18.blade.php
+++ resources/views/admin/page18.blade.php
@@ -4,11 +4,96 @@
     <h4 class="text-primary fw-bold">Chart Indicators</h4>
     <div class="col justify-content-start text-left">
         <p>
-            Candlestick pattern usually form at a market price/level where market will most likely change direction. If you understand this definition, it simply means you don't 
-            always need to pay attention to every candlestick pattern. Only pay attention to candlestick pattern at a support or resistance level in the market.
+            Chart indicators pattern usually form at a market price/level where market will most likely change direction. If you understand this definition, it simply means you don't 
+            always need to pay attention to every candlestick pattern. Only pay attention to candlestick pattern at a support or resistance level in the market. <br>
+
+            But before that, you need to know what a candlestick is. Candlestick is object that represent market price and its movement. Below image is a 
+            is a typical candlestick.
             
             <div class="d-flex justify-content-center mt-3">
-                <img src="{{ asset("../images/trendline.png") }}" alt="" style="width: 60%;">
+                <img src="{{ asset("../images/candlestick.png") }}" alt="" style="width: 60%;">
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="carousel-inner">
+                        <div class="card-group">
+                            <div class="card">
+                            <!--<img src="..." class="card-img-top" alt="...">-->
+                            <img class="card-img-top" src="{{ asset("../images/inverted-hammer.png") }}" alt="Widget" style="width:100%; height: 100%;">
+                            {{-- <iframe frameborder="0" width="100%" height="140" src="https://www.mql5.com/en/signals/widget/signal/5cug?f=0"></iframe> --}}
+                            <div class="card-body">
+                                <h5 class="card-title">Details</h5>
+                                <p class="card-text">Few informations such as drawdown and current ROI% can be seen in above image, all informations you need to convince you about the profitability can be seen by clicking "See Results", it will take you MyFxBook.</p>
+                            </div>
+                            </div>
+                            <div class="card ms-2 border">
+                            <!--<img src="..." class="card-img-top" alt="...">-->
+                            <img class="card-img-top" src="{{ asset("../images/hanging-man.png") }}" alt="Widget" style="width:100%; height: 100%;">
+                            <div class="card-body">
+                                <h5 class="card-title">Details</h5>
+                                <p class="card-text">Few informations such as drawdown and current ROI% can be seen in above image, all informations you need to convince you about the profitability can be seen by clicking "See Results", it will take you MyFxBook.</p>
+                            </div>
+                            </div>
+                            <div class="card ms-2 border">
+                            <!--<img src="..." class="card-img-top" alt="...">-->
+                            <img class="card-img-top" src="{{ asset("../images/hammer.png") }}" alt="Widget" style="width:100%; height: 100%;">
+                            <div class="card-body">
+                                <h5 class="card-title">Details</h5>
+                                <p class="card-text">Few informations such as drawdown and current ROI% can be seen in above image, all informations you need to convince you about the profitability can be seen by clicking "See Results", it will take you MyFxBook.</p>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="card-group mt-2">
+                            <div class="card">
+                            <!--<img src="..." class="card-img-top" alt="...">-->
+                            <img class="card-img-top" src="{{ asset("../images/evening-star.png") }}" alt="Widget" style="width:100%; height: 100%;">
+                            {{-- <iframe frameborder="0" width="100%" height="140" src="https://www.mql5.com/en/signals/widget/signal/5cug?f=0"></iframe> --}}
+                            <div class="card-body">
+                                <h5 class="card-title">Details</h5>
+                                <p class="card-text">Few informations such as drawdown and current ROI% can be seen in above image, all informations you need to convince you about the profitability can be seen by clicking "See Results", it will take you MyFxBook.</p>
+                            </div>
+                            </div>
+                            <div class="card ms-2 border">
+                            <!--<img src="..." class="card-img-top" alt="...">-->
+                            <img class="card-img-top" src="{{ asset("../images/shooting-star.png") }}" alt="Widget" style="width:100%; height: 100%;">
+                            <div class="card-body">
+                                <h5 class="card-title">Details</h5>
+                                <p class="card-text">Few informations such as drawdown and current ROI% can be seen in above image, all informations you need to convince you about the profitability can be seen by clicking "See Results", it will take you MyFxBook.</p>
+                            </div>
+                            </div>
+                            <div class="card ms-2 border">
+                            <!--<img src="..." class="card-img-top" alt="...">-->
+                            <img class="card-img-top" src="{{ asset("../images/morning-star.png") }}" alt="Widget" style="width:100%; height: 100%;">
+                            <div class="card-body">
+                                <h5 class="card-title">Details</h5>
+                                <p class="card-text">Few informations such as drawdown and current ROI% can be seen in above image, all informations you need to convince you about the profitability can be seen by clicking "See Results", it will take you MyFxBook.</p>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="card-group mt-2">
+                            <div class="card">
+                            <!--<img src="..." class="card-img-top" alt="...">-->
+                            <img class="card-img-top" src="{{ asset("../images/bullish-engulfing.png") }}" alt="Widget" style="width:100%; height: 100%;">
+                            {{-- <iframe frameborder="0" width="100%" height="140" src="https://www.mql5.com/en/signals/widget/signal/5cug?f=0"></iframe> --}}
+                            <div class="card-body">
+                                <h5 class="card-title">Details</h5>
+                                <p class="card-text">Few informations such as drawdown and current ROI% can be seen in above image, all informations you need to convince you about the profitability can be seen by clicking "See Results", it will take you MyFxBook.</p>
+                            </div>
+                            </div>
+                            <div class="card ms-2 border">
+                            <!--<img src="..." class="card-img-top" alt="...">-->
+                            <img class="card-img-top" src="{{ asset("../images/bearish-engulfing.png") }}" alt="Widget" style="width:100%; height: 100%;">
+                            <div class="card-body">
+                                <h5 class="card-title">Details</h5>
+                                <p class="card-text">Few informations such as drawdown and current ROI% can be seen in above image, all informations you need to convince you about the profitability can be seen by clicking "See Results", it will take you MyFxBook.</p>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </p>
