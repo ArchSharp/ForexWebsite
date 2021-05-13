fbd98a9d8483bf2b279897dfcdf2c776e5c57b36
ArchSharp <56973306+ArchSharp@users.noreply.github.com>
2021-05-13 03:15:10 +0100

    tutorial

--- resources/views/admin/page15.blade.php
+++ resources/views/admin/page15.blade.php
@@ -14,10 +14,10 @@
                 </li>
             </ul>
             <h5>How to identify a true Support and Resistance</h5>
-            <i class="fa fa-skull-crossbones text-danger fs-3 text-success" aria-hidden="true"></i> It is not a support just because you think market has fell significantly neither is it a reesistance 
+            <i class="fa fa-skull-crossbones text-danger fs-3" aria-hidden="true"></i> It is not a support just because you think market has fell significantly neither is it a reesistance 
             just because you think market has rise significantly. <i class="fa fa-skull-crossbones text-danger fs-3 text-success" aria-hidden="true"></i> <br>
 
-            <i class="fa fa-skull-crossbones text-danger fs-3 text-success" aria-hidden="true"></i> It is a support if the bottom of 3 to 4 or more candles are touching a particular market price and refuse to break the price downward. It is a Resistance if the top of 3 to 4 or more candles are touching a particular market price and refuse to break the price upward.<i class="fa fa-skull-crossbones text-danger fs-3 text-success" aria-hidden="true"></i> <br>
+            <i class="fa fa-check-square fs-3 text-success" aria-hidden="true"></i> It is a support if the bottom of 3 to 4 or more candles are touching a particular market price and refuse to break the price downward. It is a Resistance if the top of 3 to 4 or more candles are touching a particular market price and refuse to break the price upward.<i class="fa fa-check-square fs-3 text-success" aria-hidden="true"></i> <br>
             Note: The more the candles that are touching a particular markets indicates the stronger the resistance or support.
             <div class="d-flex justify-content-center mt-3">
                 <img src="{{ asset("../images/S-R.png") }}" alt="" style="width: 70%;">
