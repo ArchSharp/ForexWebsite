d9d27813d0604d662d4c12c4f3884668fa7945f0
ArchSharp <56973306+ArchSharp@users.noreply.github.com>
2021-05-16 16:16:54 +0100

    tutorial

--- resources/views/layouts/dashPages/header.blade.php
+++ resources/views/layouts/dashPages/header.blade.php
@@ -14,5 +14,162 @@
                 <a class="dropdown-item" aria-current="page" href="{{ route('auth.resetpassword') }}">Reset Password</a>
               </ul>
             </li>
+            <li class="nav-item">
+              <ul class="nav flex-column">
+                <a class="nav-link" data-bs-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample" aria-current="page" href="#">Forex - Beginners   +</a>
+                <div class="collapse" id="collapseExample1">
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.dashboard') }}" onclick="return show('Page2','Page1');">
+                      Definition
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page2') }}" onclick="return show('Page3',value);">
+                      Major Currencies
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page3') }}" onclick="return show('Page3',value);">
+                      Currency pairs
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page4') }}" onclick="return show('Page3',value);">
+                      Forex Terminologies
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page5') }}" onclick="return show('Page3',value);">
+                      Forex Order Types
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page6') }}" onclick="return show('Page3',value);">
+                      Forex Trading Time (or Session)
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page7') }}" onclick="return show('Page3',value);">
+                      Forex Participants
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page8') }}" onclick="return show('Page3',value);">
+                      Account Metrics and Meanings
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page9') }}" onclick="return show('Page3',value);">
+                      Brokers
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page10') }}" onclick="return show('Page3',value);">
+                      Factors: Broker Selection
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page11') }}" onclick="return show('Page3',value);">
+                      Open Forex Trading Account: How?
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page12') }}" onclick="return show('Page3',value);">
+                      Market Analysis
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page13') }}" onclick="return show('Page3',value);">
+                      Video Recap
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page14') }}" onclick="return show('Page3',value);">
+                      Remarks
+                    </a>
+                  </li>
+                </div>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <ul class="nav flex-column">
+                <a class="nav-link" data-bs-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample" aria-current="page" href="#" onclick="return show('Page1','Page2');">Forex - Intermediate   +</a>
+                <div class="collapse" id="collapseExample2">
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page15') }}">
+                      Support and Resistance
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page16') }}" onclick="return show('Page3',value);">
+                      Trend and Trend channels
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page17') }}" onclick="return show('Page3',value);">
+                      Candlestick pattern
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page18') }}" onclick="return show('Page3',value);">
+                      Chart Indicators
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page19') }}" onclick="return show('Page3',value);">
+                      Chart Patterns
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page20') }}" onclick="return show('Page3',value);">
+                      Divergence
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page21') }}" onclick="return show('Page3',value);">
+                      Fundamental Analysis
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page22') }}" onclick="return show('Page3',value);">
+                      Sentimental Analysis
+                    </a>
+                  </li>
+                </div>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <ul class="nav flex-column">
+                <a class="nav-link" data-bs-toggle="collapse" href="#collapseExample3" role="button" aria-expanded="false" aria-controls="collapseExample" aria-current="page" href="#" onclick="return show('Page1','Page2');">Forex - Advanced   +</a>
+                <div class="collapse" id="collapseExample3">
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page23') }}">
+                      Correlation
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{ route('page24') }}" onclick="return show('Page3',value);">
+                      Risk management
+                    </a>
+                  </li>
+                </div>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <ul class="nav flex-column">
+                <a class="nav-link" data-bs-toggle="collapse" href="#collapseExample4" role="button" aria-expanded="false" aria-controls="collapseExample" aria-current="page" href="#" onclick="return show('Page1','Page2');">Our strategy   +</a>
+                <div class="collapse" id="collapseExample4">
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">
+                      Disclaimer
+                    </a>
+                  </li><li class="nav-item">
+                    <a class="nav-link" href="#" onclick="return show('Page3',value);">
+                      Our Strategy
+                    </a>
+                  </li>
+                </div>
+              </ul>
+            </li>
           </ul>
         </div>
