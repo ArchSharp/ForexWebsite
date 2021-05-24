3b225ba032cf0447866e84b477933d97b509d4f1
ArchSharp <56973306+ArchSharp@users.noreply.github.com>
2021-05-24 10:37:25 +0100

    Google analytics

---
+++ resources/views/ArchfxglobeAnalytics.blade.php
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-EEWS4ZFXXV"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-EEWS4ZFXXV');
+</script>
