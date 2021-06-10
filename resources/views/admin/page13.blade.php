a32a7c662630e6ecb669b3b1176f014eff570702
ArchSharp <56973306+ArchSharp@users.noreply.github.com>
2021-06-10 18:17:07 +0100

    update

--- resources/views/admin/page13.blade.php
+++ resources/views/admin/page13.blade.php
@@ -29,6 +29,29 @@
                                 </div>
                             </div>
                         </div>
+
+                        <div class="card-group">
+                            <div class="card">
+                                <!--<img src="..." class="card-img-top" alt="...">-->
+                                {{-- <img class="card-img-top" src="{{ asset("../images/inverted-hammer.png") }}" alt="Widget" style="width:100%; height: 100%;"> --}}
+                                {{-- <iframe frameborder="0" width="100%" height="140" src="https://www.mql5.com/en/signals/widget/signal/5cug?f=0"></iframe> --}}
+                                <iframe class="d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block" style="width:100%;" height="480" src="https://www.youtube.com/embed/CVel0yi-uAo?list=PLhPsoJ0uzsOpDmfbtykbGpMJ3z36XjuVK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                <div class="text-center">
+                                    <a class="d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none fw-bold" href="https://www.youtube.com" target="_blank">Forex Beginners</a>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title text-primary fw-bold">Summary</h5>
+                                    <p class="card-text">Carefully watch this video, 
+                                        <span class="d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none">
+                                            Click the video link above it will take you to the <a href="#" class="text-primary fw-bold">Youtube</a> video, <span class="fw-bold text-primary">There is a bonus for those who subscribe, comment and like after watching the video</span>.
+                                        </span>
+                                        <span class="d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block">
+                                            Go to <a href="https://www.youtube.com" target="_blank" class="text-primary fw-bold">Youtube</a> to subscribe, comment and like <span class="fw-bold text-primary">to qualify for a bonus</span> .
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
