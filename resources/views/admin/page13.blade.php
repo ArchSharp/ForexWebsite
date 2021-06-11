07f6918335c459a2bd77e5659d0d2b3db08e0e2b
ArchSharp <56973306+ArchSharp@users.noreply.github.com>
2021-06-11 01:35:11 +0100

    update

--- resources/views/admin/page13.blade.php
+++ resources/views/admin/page13.blade.php
@@ -12,9 +12,9 @@
                                 <!--<img src="..." class="card-img-top" alt="...">-->
                                 {{-- <img class="card-img-top" src="{{ asset("../images/inverted-hammer.png") }}" alt="Widget" style="width:100%; height: 100%;"> --}}
                                 {{-- <iframe frameborder="0" width="100%" height="140" src="https://www.mql5.com/en/signals/widget/signal/5cug?f=0"></iframe> --}}
-                                <iframe class="d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block" style="width:100%;" height="480" src="https://www.youtube.com/embed/CVel0yi-uAo?list=PLhPsoJ0uzsOpDmfbtykbGpMJ3z36XjuVK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                <iframe class="d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block" style="width:100%;" height="480" src="https://www.youtube.com/embed/IQArgpbjFHg?list=PLhPsoJ0uzsOpDmfbtykbGpMJ3z36XjuVK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                                 <div class="text-center">
-                                    <a class="d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none fw-bold" href="https://www.youtube.com" target="_blank">Forex Beginners</a>
+                                    <a class="d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none fw-bold" href="https://www.youtube.com/watch?v=IQArgpbjFHg&list=PLhPsoJ0uzsOpDmfbtykbGpMJ3z36XjuVK&index=1" target="_blank">Forex Intermediate Video-1</a>
                                 </div>
                                 <div class="card-body">
                                     <h5 class="card-title text-primary fw-bold">Summary</h5>
@@ -23,21 +23,21 @@
                                             Click the video link above it will take you to the <a href="#" class="text-primary fw-bold">Youtube</a> video, <span class="fw-bold text-primary">There is a bonus for those who subscribe, comment and like after watching the video</span>.
                                         </span>
                                         <span class="d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block">
-                                            Go to <a href="https://www.youtube.com" target="_blank" class="text-primary fw-bold">Youtube</a> to subscribe, comment and like <span class="fw-bold text-primary">to qualify for a bonus</span> .
+                                            Go to <a href="https://www.youtube.com/playlist?list=PLhPsoJ0uzsOrGHesz3pZiOoY30G9dJFWk" target="_blank" class="text-primary fw-bold">Youtube</a> to subscribe, comment and like <span class="fw-bold text-primary">to qualify for a bonus</span> .
                                         </span>
                                     </p>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="card-group">
+                        <div class="card-group mt-4">
                             <div class="card">
                                 <!--<img src="..." class="card-img-top" alt="...">-->
                                 {{-- <img class="card-img-top" src="{{ asset("../images/inverted-hammer.png") }}" alt="Widget" style="width:100%; height: 100%;"> --}}
                                 {{-- <iframe frameborder="0" width="100%" height="140" src="https://www.mql5.com/en/signals/widget/signal/5cug?f=0"></iframe> --}}
-                                <iframe class="d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block" style="width:100%;" height="480" src="https://www.youtube.com/embed/CVel0yi-uAo?list=PLhPsoJ0uzsOpDmfbtykbGpMJ3z36XjuVK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                <iframe class="d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block" style="width:100%;" height="480" src="https://www.youtube.com/embed/TuxjSvw0mqY?list=PLhPsoJ0uzsOpDmfbtykbGpMJ3z36XjuVK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                                 <div class="text-center">
-                                    <a class="d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none fw-bold" href="https://www.youtube.com" target="_blank">Forex Beginners</a>
+                                    <a class="d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none fw-bold" href="https://www.youtube.com/watch?v=TuxjSvw0mqY&list=PLhPsoJ0uzsOpDmfbtykbGpMJ3z36XjuVK&index=2" target="_blank">Forex Intermediate Video-2</a>
                                 </div>
                                 <div class="card-body">
                                     <h5 class="card-title text-primary fw-bold">Summary</h5>
@@ -46,7 +46,76 @@
                                             Click the video link above it will take you to the <a href="#" class="text-primary fw-bold">Youtube</a> video, <span class="fw-bold text-primary">There is a bonus for those who subscribe, comment and like after watching the video</span>.
                                         </span>
                                         <span class="d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block">
-                                            Go to <a href="https://www.youtube.com" target="_blank" class="text-primary fw-bold">Youtube</a> to subscribe, comment and like <span class="fw-bold text-primary">to qualify for a bonus</span> .
+                                            Go to <a href="https://www.youtube.com/playlist?list=PLhPsoJ0uzsOrGHesz3pZiOoY30G9dJFWk" target="_blank" class="text-primary fw-bold">Youtube</a> to subscribe, comment and like <span class="fw-bold text-primary">to qualify for a bonus</span> .
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card-group mt-4">
+                            <div class="card">
+                                <!--<img src="..." class="card-img-top" alt="...">-->
+                                {{-- <img class="card-img-top" src="{{ asset("../images/inverted-hammer.png") }}" alt="Widget" style="width:100%; height: 100%;"> --}}
+                                {{-- <iframe frameborder="0" width="100%" height="140" src="https://www.mql5.com/en/signals/widget/signal/5cug?f=0"></iframe> --}}
+                                <iframe class="d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block" style="width:100%;" height="480" src="https://www.youtube.com/embed/j9jKRqT3iGA?list=PLhPsoJ0uzsOpDmfbtykbGpMJ3z36XjuVK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                <div class="text-center">
+                                    <a class="d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none fw-bold" href="https://www.youtube.com/watch?v=j9jKRqT3iGA&list=PLhPsoJ0uzsOpDmfbtykbGpMJ3z36XjuVK&index=3" target="_blank">Forex Intermediate Video-3</a>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title text-primary fw-bold">Summary</h5>
+                                    <p class="card-text">Carefully watch this video, 
+                                        <span class="d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none">
+                                            Click the video link above it will take you to the <a href="#" class="text-primary fw-bold">Youtube</a> video, <span class="fw-bold text-primary">There is a bonus for those who subscribe, comment and like after watching the video</span>.
+                                        </span>
+                                        <span class="d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block">
+                                            Go to <a href="https://www.youtube.com/playlist?list=PLhPsoJ0uzsOrGHesz3pZiOoY30G9dJFWk" target="_blank" class="text-primary fw-bold">Youtube</a> to subscribe, comment and like <span class="fw-bold text-primary">to qualify for a bonus</span> .
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card-group mt-4">
+                            <div class="card">
+                                <!--<img src="..." class="card-img-top" alt="...">-->
+                                {{-- <img class="card-img-top" src="{{ asset("../images/inverted-hammer.png") }}" alt="Widget" style="width:100%; height: 100%;"> --}}
+                                {{-- <iframe frameborder="0" width="100%" height="140" src="https://www.mql5.com/en/signals/widget/signal/5cug?f=0"></iframe> --}}
+                                <iframe class="d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block" style="width:100%;" height="480" src="https://www.youtube.com/embed/tdKbiqzhNmE?list=PLhPsoJ0uzsOpDmfbtykbGpMJ3z36XjuVK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                <div class="text-center">
+                                    <a class="d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none fw-bold" href="https://www.youtube.com/watch?v=tdKbiqzhNmE&list=PLhPsoJ0uzsOpDmfbtykbGpMJ3z36XjuVK&index=4" target="_blank">Forex Intermediate Video-4</a>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title text-primary fw-bold">Summary</h5>
+                                    <p class="card-text">Carefully watch this video, 
+                                        <span class="d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none">
+                                            Click the video link above it will take you to the <a href="#" class="text-primary fw-bold">Youtube</a> video, <span class="fw-bold text-primary">There is a bonus for those who subscribe, comment and like after watching the video</span>.
+                                        </span>
+                                        <span class="d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block">
+                                            Go to <a href="https://www.youtube.com/playlist?list=PLhPsoJ0uzsOrGHesz3pZiOoY30G9dJFWk" target="_blank" class="text-primary fw-bold">Youtube</a> to subscribe, comment and like <span class="fw-bold text-primary">to qualify for a bonus</span> .
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card-group mt-4">
+                            <div class="card">
+                                <!--<img src="..." class="card-img-top" alt="...">-->
+                                {{-- <img class="card-img-top" src="{{ asset("../images/inverted-hammer.png") }}" alt="Widget" style="width:100%; height: 100%;"> --}}
+                                {{-- <iframe frameborder="0" width="100%" height="140" src="https://www.mql5.com/en/signals/widget/signal/5cug?f=0"></iframe> --}}
+                                <iframe class="d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block" style="width:100%;" height="480" src="https://www.youtube.com/embed/tF_ns17XRPw?list=PLhPsoJ0uzsOpDmfbtykbGpMJ3z36XjuVK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                <div class="text-center">
+                                    <a class="d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none fw-bold" href="https://www.youtube.com/watch?v=tF_ns17XRPw&list=PLhPsoJ0uzsOpDmfbtykbGpMJ3z36XjuVK&index=5" target="_blank">Forex Intermediate Video-4</a>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title text-primary fw-bold">Summary</h5>
+                                    <p class="card-text">Carefully watch this video, 
+                                        <span class="d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none">
+                                            Click the video link above it will take you to the <a href="#" class="text-primary fw-bold">Youtube</a> video, <span class="fw-bold text-primary">There is a bonus for those who subscribe, comment and like after watching the video</span>.
+                                        </span>
+                                        <span class="d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block">
+                                            Go to <a href="https://www.youtube.com/playlist?list=PLhPsoJ0uzsOrGHesz3pZiOoY30G9dJFWk" target="_blank" class="text-primary fw-bold">Youtube</a> to subscribe, comment and like <span class="fw-bold text-primary">to qualify for a bonus</span> .
                                         </span>
                                     </p>
                                 </div>
